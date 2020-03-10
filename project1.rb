771cbf498b755f055f7040ac594f41ca9655f30c
AndrewTranDev <tran@ATran-PC.localdomain>
2020-03-10 11:13:00 +1100

    added dictionary_lookup gem

--- project1.rb
+++ project1.rb
@@ -4,10 +4,15 @@
 #determine how many games to play?
 #how to check if words are correct?
 
+#app gives random amount of letters
+
+#ruby gems dictionary
+
+#searches internet to see if word exists
+#points system
+#output score onto text file and read it at start game
 
 
-
-
-
-
-
+require 'dictionary_lookup'
+results = DictionaryLookup::Base.define("hellop")
+p results
