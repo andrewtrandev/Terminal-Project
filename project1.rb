8d9375146eb30dfe861e48ab629dadd1be7ddec9
AndrewTranDev <tran@ATran-PC.localdomain>
2020-03-10 21:39:26 +1100

    added colorize to text and modified vowel adder

--- project1.rb
+++ project1.rb
@@ -12,41 +12,50 @@
 #points system
 #output score onto ttext file and read it at start game
 #could also have a play again function
+require 'dictionary_lookup'
+require 'tty-prompt'
+require 'colorize'
+# [:black, :light_black, :red, :light_red, :green, :light_green, :yellow, :light_yellow, :blue, 
+#     :light_blue, :magenta, :light_magenta, :cyan, :light_cyan, :white, :light_white, :default]
 
 play_game=true
 
 while play_game==true
 
-    require 'dictionary_lookup'
-
-
-    p "Welcome to terminal scrabble,"
-    p   "please try to come up with the longest word possible"
-    p   "for the most points"
-    p "How many letters would you like to scramble?"
+    puts "Welcome to terminal scrabble,".colorize(:blue)
+    puts  "please try to come up with the longest word possible".colorize(:blue)
+    puts  "for the most points".colorize(:blue)
+    puts "How many letters would you like to scramble?".colorize(:blue)
     #input for how many letters to scramble
-    numberofletters=gets.chomp
+    numberofletters=gets.chomp.to_i
 
 
     #returns an array of shuffle letters
     #should turn into function?
     #at the moment it's only shuffling 1 set of 26 letters
     def lettershuffle(numberofletters)
-        letters =('a'..'z').to_a.shuffle[0,((numberofletters.to_i)-1)].join
+        
+       
         #incase there is no vowels, vowel adder
-        vowels = ['a', 'i', 'o', 'e', 'u'].shuffle[1]
-        # p vowels
-        # p letters
+        if numberofletters< 6 
+        #for letters a to z > convert to array and shuffle 
+        letters =('a'..'z').to_a.shuffle[0,(numberofletters-1)].join
+        vowels = ['a', 'i', 'o', 'e', 'u'].shuffle[0]
+        else
+            letters =('a'..'z').to_a.shuffle[0,(numberofletters-2)].join
+            vowels = ['a', 'i', 'o', 'e', 'u'].shuffle[0..1].join
+        end
+        p vowels
+        p letters
         return vowels + letters  
     end
 
     #assign randomletters using method
     p randomletters=lettershuffle(numberofletters)
-    p "Please enter the longest word you can think of"
+    puts "Please enter the longest word you can think of".colorize(:red)
     
     # validword=true
     # if validword == true
-
 
 
     userinput = gets.chomp
@@ -57,21 +66,19 @@ while play_game==true
     #for each object in userinput check
     p randomletterscheck
     p userinputcheck
-
-
+    
     
     for letters in userinputcheck
         #if randomletters had the letter inside
         if randomletterscheck.include?(letters)
            randomletterscheck.delete(letters)
         else
-            p "not a valid word"
+            puts "Not a valid word!".colorize(:yellow)
             validword=false
             break
         end
     end
     
-
     #need to check if userinput is included within random letters
     
 
@@ -79,20 +86,22 @@ while play_game==true
     #feed user input in that checks with dictionary
     #sanitize inputs for get rid of numbers and odd characters?
 
-
     results = DictionaryLookup::Base.define(userinput)
     results2 = DictionaryLookup::Base.define(userinput.chop) #chop cuts off the last character
 
     #word length = points length
     #if empty array then no points
-
+    #if answer is not equal to empty array then award userinput.length as points
+    
     if results != [] || results2!=[]
-        p "Congratulations you win #{userinput.length} points"
+        puts "Congratulations you win #{userinput.length} points".colorize(:black).colorize(:background =>:yellow)
+        score = userinput.length
+        puts "score:#{score}"
     else
-        p "No points"
+        puts "No points".colorize(:light_magenta)
     end
 
-        p "Would you like to play again? (y/n)"
+        puts "Would you like to play again? (y/n)".colorize(:yellow)
         play_game = gets.chomp
         #ternary if userinput = y go back to top and continue running game
         if play_game == 'y' ? play_game=true : play_game=false
---
+++ test.rb
@@ -0,0 +1,7 @@
+vowels = ['a', 'i', 'o', 'e', 'u'].shuffle[1]
+vowels2 = ['a', 'i', 'o', 'e', 'u'].shuffle[0..4]  #what does this modifier even do // returns nil at 5, ohhh it must shuffle the array and return
+#whatever that index position is
+
+
+p vowels
+p vowels2
